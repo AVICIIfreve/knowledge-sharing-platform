f416d4647a4b2b30d7d1dfa95d193bdaf93469b4
wzy <759799349@qq.com>
2024-12-03 16:26:54 +0800

    Router achievement

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import QuestionDetail from "./pages/QuestionDetail";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/question/:id" element={<QuestionDetail />}></Route>
+        <Route path="/profile/:username" element={<UserProfile />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
