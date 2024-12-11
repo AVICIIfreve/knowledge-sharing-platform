73bf0a4d9b88f747e8be8edec3c2bf6acdd40325
wzy <759799349@qq.com>
2024-12-11 17:40:31 +0800

    Front-end and back-end separation (backend normal)+ignore .env

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5100;
 
 // 中间件
 app.use(express.json());
