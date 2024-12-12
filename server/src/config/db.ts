c5c359bbabcd32132e71953e3b5c2843501b7dae
wzy <759799349@qq.com>
2024-12-12 16:05:32 +0800

    (Backend)  Database  import

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,7 +1,19 @@
 import mysql from 'mysql2/promise' //导入MySQL驱动
+import dotenv from 'dotenv';
 
+//加载 .env文件中的环境变量
+dotenv.config();
 
 //创建数据库连接池
 const pool = mysql.createPool({
-    
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT) || 3306,
+    waitForConnections: true,
+    connectionLimit: 10, // 最大连接数
+    queueLimit: 0,       // 队列最大数量（0 表示不限制）
 })
+
+export default pool;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import pool from './config/db'
 import dotenv from 'dotenv';
 
 // 加载 .env 文件的环境变量
@@ -14,6 +15,29 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+async function testDatabase() {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Database connection successful!")
+    connection.release();
+  }catch(error){
+    console.error("Database connection failed:",error);
+    process.exit(1);
+  }
+}
+
+testDatabase();
+
+app.get('/users', async(req,res)=>{
+  try{
+    const [rows] = await pool.query("SELECT * FROM users");
+    res.json(rows);
+  }catch(error){
+    console.error('Error querying the database:', error);
+    res.status(500).send("Database query failed")
+  }
+})
 
 // 启动服务器
 app.listen(PORT, () => {
